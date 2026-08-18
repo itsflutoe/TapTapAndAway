2b0a03c8059a2950944779946faf3d1e3f6d5d40
itsflutoe <charliedelacruz0727@gmail.com>
2026-08-18 15:11:58 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import './styles/global.css';
 import LoginPage from './pages/LoginPage';

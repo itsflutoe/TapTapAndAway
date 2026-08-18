0b9346bacf07bee1d0b1b3c19d2773d6380a7f05
itsflutoe <charliedelacruz0727@gmail.com>
2026-08-18 11:32:24 +0800

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { initInstallPromptCapture, registerServiceWorker } from './lib/pwa';
+
+registerServiceWorker();
+initInstallPromptCapture();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

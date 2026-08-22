348c4dd302b875071d119e2cc8d267de5eb24375
itsflutoe <charliedelacruz0727@gmail.com>
2026-08-23 00:00:58 +0800

    Delete src/components directory

--- src/components/LoadingScreen.tsx
+++
@@ -1,9 +0,0 @@
-export default function LoadingScreen() {
-  return (
-    <div className="loading-screen">
-      <div className="brand-mark">🐦</div>
-      <div style={{ fontWeight: 800, fontSize: 18 }}>Tap Tap and Away</div>
-      <div className="spinner" />
-    </div>
-  );
-}
